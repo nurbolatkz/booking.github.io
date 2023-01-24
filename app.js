60384a82c933fdd27e1c677da5a0b49e53bb3e33
nurbolatkz <nurbolat.duisenbek@narxoz.kz>
2023-01-24 15:07:41 +0600

    test form

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ let show_btn = document.getElementById('show_btn');
 let p = document.createElement('p');
 
 show_btn.addEventListener('click', function() {
-    let user_name = document.getElementById('user_name');
-    let user_phone_number = document.getElementById('user_phone_number');
+    let user_name = document.getElementById('user_name').value;
+    let user_phone_number = document.getElementById('user_phone_number').value;
 
     p.innerText = 'User Name ---- ' + user_name + 'User phone number --- ' + user_phone_number;
+    tg.MainButton.show();
 });
 
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
--- keyboards.py
+++ keyboards.py
@@ -1,7 +1,10 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton, 
+                        WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup)
 from aiogram.utils.callback_data import CallbackData
 
-web_app = WebAppInfo(url="https://habr.com/ru/post/666278/")
+from config import WEB_URL
+
+web_app = WebAppInfo(url=WEB_URL)
 
 keyboard = ReplyKeyboardMarkup(
     keyboard=[
